fc6fe8015e40f279305d684495d4c8c69b0a3a7b
ajosepha <ariel.isaacs@gmail.com>
2013-11-15 06:59:17 -0500

    add updated game

--- Number-Guesser/spec/number-guessor_spec.rb
+++ Number-Guesser/spec/number-guessor_spec.rb
@@ -1,1 +1,17 @@
-require_relative '../number-guessor.rb'
+require_relative '../number-guesser.rb'
+
+describe number-guessor, "#play" do 
+
+	it "Should return an integer" do
+		expect(Play.new.play_computer).to_be_a(Integer)
+	end
+	
+	it "should return an number greater than 0" do	
+		Play.new.play_computer.should to_be >0
+	end
+	
+	it "should return a number less than 7" do
+		Play.new.play_computer.should be < 100
+	end
+	
+end		
